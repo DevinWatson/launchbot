c98e14064b448ba050602da04aafc746f919b96f
The Tarvis <wilson.travis.t@gmail.com>
2017-02-02 17:14:46 -0500

    Added slack into data call so we can setup a cron type task.

--- server/data.js
+++ server/data.js
@@ -1,5 +1,5 @@
 var http = require('http');
-
+var slackService = require('./slack');
 //Create a server
 var server = http.createServer(function (request, res) {
     // Set CORS headers
@@ -7,7 +7,10 @@ var server = http.createServer(function (request, res) {
     res.setHeader('Access-Control-Request-Method', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    return handleRequest(request, res);
+    console.log(slackService.sendMessage("Test"));
+    // response = slackService.sendMessage("Test");
+    res.end("hih");
+    return res;
 
 });
 //Lets define a port we want to listen to
@@ -17,6 +20,5 @@ server.listen(PORT, function () {
     console.log("Server listening on: http://localhost:%s", PORT);
 
     console.log("Sending request to get new data")
-    http.requ
-
+    // slackService.sendMessage("test");
 });
--- server/slack.js
+++ server/slack.js
@@ -1,16 +1,27 @@
 var Slack = require('slack-node');
+
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('../resources/properties.ini');
-
 webhookUri = properties.get('main.slack.webhook.uri');
 
 slack = new Slack();
 slack.setWebhook(webhookUri);
 
-slack.webhook({
-    channel: "#general",
-    username: "webhookbot",
-    text: "This is a launch alert"
-}, function(err, response) {
-    console.log(response);
-});
+module.exports = {
+    sendMessage: function (message) {
+        return slack.webhook({
+            channel: "#general",
+            username: "webhookbot",
+            text: message
+        }, function (err, response) {
+            if (err) {
+                // console.log("Error sending message: " + err + ". Response: " + response);
+                return err;
+            } else {
+                // console.log(response);
+                return response;
+            }
+        });
+    }
+};
+
