a7c5da35c74bb5ec8bf1437386f602cec1fa14ed
The Tarvis <wilson.travis.t@gmail.com>
2017-02-02 18:48:30 -0500

    Trying to pull data from api but getting 403s. Something something headers aren't right but no idea what theyshoudl be.

--- server/data.js
+++ server/data.js
@@ -1,4 +1,7 @@
+"use strict";
+
 var http = require('http');
+var https = require('https');
 var slackService = require('./slack');
 //Create a server
 var server = http.createServer(function (request, res) {
@@ -7,9 +10,10 @@ var server = http.createServer(function (request, res) {
     res.setHeader('Access-Control-Request-Method', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    console.log(slackService.sendMessage("Test"));
+
+    // console.log(slackService.sendMessage("Test"));
     // response = slackService.sendMessage("Test");
-    res.end("hih");
+    test();
     return res;
 
 });
@@ -22,3 +26,72 @@ server.listen(PORT, function () {
     console.log("Sending request to get new data")
     // slackService.sendMessage("test");
 });
+
+function test(){
+
+        var options = {
+        host: 'launchlibrary.net',
+        // port: 80,
+        path: '/1.2/launch',
+        method: 'GET',
+        headers: {
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, sdch, br",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Cache-Control":"no-cache",
+            "Connection":"keep-alive",
+            "Host":"launchlibrary.net",
+            "Pragma":"no-cache",
+            "Upgrade-Insecure-Requests":"1"
+        }
+    };
+
+    http.get(options, (res) => {
+        const statusCode = res.statusCode;
+        const contentType = res.headers['content-type'];
+
+        let error;
+        if (statusCode !== 200) {
+            error = new Error(`Request Failed.\n` +
+                `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+            error = new Error(`Invalid content-type.\n` +
+                `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+            console.log(error.message);
+            // consume response data to free up memory
+            res.resume();
+            return;
+        }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => rawData += chunk);
+        res.on('end', () => {
+            try {
+                let parsedData = JSON.parse(rawData);
+                console.log(parsedData);
+            } catch (e) {
+                console.log(e.message);
+            }
+        });
+    }).on('error', (e) => {
+        console.log(`Got error: ${e.message}`);
+    });
+}
+
+// function getData(callback) {
+//
+//     var req = http.request(options, function(res) {
+//         console.log(res.statusCode);
+//         res.on('data', function(d) {
+//             process.stdout.write(d);
+//         });
+//     });
+//     req.end();
+//
+//     req.on('error', function(e) {
+//         console.error(e);
+//     });
+// }
