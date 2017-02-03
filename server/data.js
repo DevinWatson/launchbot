d37ae74de3ecf1ec07f9521f96fa638565ff8203
The Tarvis <wilson.travis.t@gmail.com>
2017-02-03 11:00:07 -0500

    cleaned up the data script and using promises. Centralized the httpGet data so its just a single metho that takes in path.

--- server/data.js
+++ server/data.js
@@ -1,123 +1,21 @@
-"use strict";
 
-var http = require('http');
-var slackService = require('./slack');
+// var mysql = require("mysql");
+var http = require('./http');
 
-
-function fetchMoreData(launchId){
-    var path = '/1.2/launch/'+launchId;
-    console.log(path)
-
-    var options = {
-        host: 'launchlibrary.net',
-        path: path,
-        method: 'GET',
-        headers: {
-            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, sdch, br",
-            "Accept-Language": "en-US,en;q=0.8",
-            "Cache-Control":"no-cache",
-            "Connection":"keep-alive",
-            "Host":"launchlibrary.net",
-            "Pragma":"no-cache",
-            "Upgrade-Insecure-Requests":"1",
-            "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
-        }
-    };
-
-    http.get(options, (res) => {
-        const statusCode = res.statusCode;
-        const contentType = res.headers['content-type'];
-
-        let error;
-        if (statusCode !== 200) {
-            error = new Error(`Request Failed.\n` +
-                `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-            error = new Error(`Invalid content-type.\n` +
-                `Expected application/json but received ${contentType}`);
-        }
-        if (error) {
-            console.log(error.message);
-            // consume response data to free up memory
-            res.resume();
-            return;
-        }
-
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => rawData += chunk);
-        res.on('end', () => {
-            try {
-                let parsedData = JSON.parse(rawData);
-                // for (var i = 0, len = parsedData.launches.length; i < len; i++) {
-                //     console.log("I: " + i + parsedData.launches[i].toString() + "\n");
-                // }
-                console.log(parsedData);
-            } catch (e) {
-                console.log(e.message);
-            }
+var basePath = "/1.2/launch";
+http.httpGet(basePath).then(function(response) {
+    return response.launches;
+}, function(error) {
+    console.error("Failed!", error);
+}). then(function (launches) {
+    for (var i = 0, len = launches.length; i < len; i++) {
+        console.log("I: " + i + launches[i].toString() + "\n");
+        http.httpGet(basePath+"/"+launches[i].id).then(function (response) {
+            console.log("Response: " + response + "\n");
+        }, function (error) {
+            console.error("Failed here!", error);
         });
-    }).on('error', (e) => {
-        console.log(`Got error: ${e.message}`);
-    });
-}
-
-function fetchBasicData(){
-        var options = {
-        host: 'launchlibrary.net',
-        path: '/1.2/launch',
-        method: 'GET',
-        headers: {
-            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, sdch, br",
-            "Accept-Language": "en-US,en;q=0.8",
-            "Cache-Control":"no-cache",
-            "Connection":"keep-alive",
-            "Host":"launchlibrary.net",
-            "Pragma":"no-cache",
-            "Upgrade-Insecure-Requests":"1",
-            "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
-        }
-    };
-
-    http.get(options, (res) => {
-        const statusCode = res.statusCode;
-        const contentType = res.headers['content-type'];
-
-        let error;
-        if (statusCode !== 200) {
-            error = new Error(`Request Failed.\n` +
-                `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-            error = new Error(`Invalid content-type.\n` +
-                `Expected application/json but received ${contentType}`);
-        }
-        if (error) {
-            console.log(error.message);
-            // consume response data to free up memory
-            res.resume();
-            return;
-        }
-
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => rawData += chunk);
-        res.on('end', () => {
-            try {
-                let parsedData = JSON.parse(rawData);
-                for (var i = 0, len = parsedData.launches.length; i < len; i++) {
-                    console.log(parsedData.launches[i].id + " \n")
-                    fetchMoreData(parsedData.launches[i].id)
-                }
-                // console.log(``);
-            } catch (e) {
-                console.log(e.message);
-            }
-        });
-    }).on('error', (e) => {
-        console.log(`Got error: ${e.message}`);
-    });
-}
-
-fetchBasicData();
+    }
+}).catch(function (error) {
+    console.error("Failed, in catch!", error);
+});
