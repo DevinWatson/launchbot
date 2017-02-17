8d647b740c08850439954ba852fb7c2de6442a91
Daniel Moore <danielfs1@gmail.com>
2017-02-17 14:21:25 -0500

    mission event model

---
+++ src/main/java/com/eccff/launchbot/models/MissionEvent.java
@@ -0,0 +1,23 @@
+package com.eccff.launchbot.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@Builder
+public class MissionEvent {
+
+    @Id
+    private int id;
+    private String name;
+    private int relativeTime;
+    private int type;
+    private int duration;
+    private String description;
+    private int parentid;
+
+}
